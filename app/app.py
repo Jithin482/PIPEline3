a4a4422d684a639b7b2567d8c7801705950748e9
Jithin482 <jithinlal482@gmail.com>
2025-03-09 17:07:33 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)  # Ensure it listens on all interfaces
+    app.run(host='0.0.0.0', port=5000)  # Run on port 5000
